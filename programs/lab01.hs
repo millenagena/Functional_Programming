89dd3782498cb069c5f7a29d4e64d0ed93dec351
Millena Gena <millenagena@gmail.com>
2022-05-11 23:35:32 -0300

    Taking off some parenthesis

--- programs/lab01.hs
+++ programs/lab01.hs
@@ -9,11 +9,11 @@ last_elem list = head (take 1 (reverse list))
 
 -- exercicio 4
 last_elem2 list = head (reverse list)
-last_elem3 list = head (drop (length(list) - 1) list)
+last_elem3 list = head (drop (length list - 1) list)
 
 -- exercício 5
 remove_last list = reverse (drop 1 (reverse list))
-remove_last2 list = take (length(list) - 1) list
+remove_last2 list = take (length list - 1) list
 
 main :: IO ()
 main = do
