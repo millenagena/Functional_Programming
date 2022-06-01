96cc669440979e46d36955ea21c263d417e6228d
Millena Gená <millenagena@gmail.com>
2022-05-31 23:45:03 -0300

    up

---
+++ programs/lab01.hs
@@ -0,0 +1,11 @@
+dobro x = 2 * x
+
+quadruplo n = dobro n + dobro n
+
+soma2 x y = x + y
+
+soma4 x y z w = soma2 x y + soma2 z w
+
+misterio x y z w = soma2(soma2 x y)(soma2 z w)
+
+hipotenusa c1 c2 = sqrt (soma2 (c1*2) (c2*2))
--- programs/lab02.hs
+++ programs/lab02.hs
@@ -1,11 +1,62 @@
-dobro x = 2 * x
+----- Exercício 1 -----
 
-quadruplo n = dobro n + dobro n
+type Pessoa = (String, Int, Float, Char)
+pess :: Int -> Pessoa
+pess x
+    |x==1 = ("Cristina", 27, 1.66,'F') 
+    |x==2 = ("Flávio", 26, 1.85,'M') 
+    |x==3 = ("Mariana", 67, 1.55,'F') 
+    |x==4 = ("Cecília", 48, 1.78, 'M') 
+    |x==5 = ("Paulo", 24, 1.93, 'M') 
+    |x==6 = ("Clara", 38, 1.70,'F') 
+    |x==7 = ("Rodrigo", 12, 1.85, 'M') 
+    |x==8 = ("Giovana", 31, 1.58,'F') 
+    |x==9 = ("Daniel", 75, 1.74, 'M') 
+    |x==10 = ("Eduardo", 21, 1.69,'F') 
+    |otherwise = ("Acabou!",0, 0.0, 'x') 
 
-soma2 x y = x + y
+altura :: Pessoa -> Float
+altura (a, b, c, d) = c
 
-soma4 x y z w = soma2 x y + soma2 z w
+maisAlta :: Int -> Int -> Int
+maisAlta x y 
+    | altura(pess x) > altura(pess y) = x
+    | otherwise = y
 
-misterio x y z w = soma2(soma2 x y)(soma2 z w)
+diferencaAltura :: Int -> Int -> Float
+diferencaAltura a b 
+    | altura(pess a) > altura(pess b) = altura(pess a) - altura(pess b)
+    | otherwise = altura(pess b) - altura(pess a)
 
-hipotenusa c1 c2 = sqrt (soma2 (c1*2) (c2*2))
+-- pessoaMaisAlta :: Int -> Int
+-- pessoaMaisAlta a = c
+--     where
+--         b = maisAlta a a-1
+--         c = pessoaMaisAlta b
+
+----- Exercício 2 -----
+
+calculaDelta :: Float -> Float -> Float -> Float
+calculaDelta a b c = (b*b) - (4*a*c)
+
+calculaRaizes :: Float -> Float -> Float -> (Float, Float)
+calculaRaizes a b c = ( ((-b) + sqrt(calculaDelta a b c))/ (2*a) ,  ((-b) - sqrt(calculaDelta a b c)) / (2*a) )
+
+calculaEq :: Float -> Float -> Float -> (Float, Float)
+calculaEq a b c
+    | calculaDelta a b c < 0 = (0.0, 0.0)
+    | otherwise = calculaRaizes a b c
+
+----- Exercício 3 -----
+
+verificaTriangulo :: Float -> Float -> Float -> Bool
+verificaTriangulo a b c
+    | a < (b + c) && b < (a + c) && c < (a + b) = True
+    | otherwise = False
+
+tipoTriangulo :: Float -> Float -> Float -> String
+tipoTriangulo x y z
+    | x == y && x == z && verificaTriangulo x y z = "Equilatero"
+    | (x == y || x == z || y == z) && verificaTriangulo x y z = "Isosceles"
+    | x /= y && y /= z && verificaTriangulo x y z = "Escaleno"
+    | otherwise = "Nao eh um triangulo"
